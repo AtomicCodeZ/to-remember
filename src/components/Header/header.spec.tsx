a3b12e91ca13eeda707281365ec04faa39452f52
NeoticoZ <heriksaraujodias06@gmail.com>
2022-09-02 13:37:17 -0300

    feat: Header tests implemented

--- src/components/Header/header.spec.tsx
+++ src/components/Header/header.spec.tsx
@@ -2,11 +2,55 @@ import { render, screen } from "@testing-library/react";
 import { Header } from ".";
 
 describe("Header", () => {
-  it("Renders correctly", () => {
+  it("Renders logo correctly", () => {
     render(<Header />);
 
     const logo = screen.getByText("Animados");
 
     expect(logo).toBeInTheDocument();
+  });
+
+  it("Redirects to the main page when the logo is clicked", () => {
+    render(<Header />);
+
+    const logo = screen.getByText("Animados");
+
+    expect(logo.getAttribute("href")).toEqual("/");
+  });
+
+  it("Renders navigation links correctly", () => {
+    render(<Header />);
+
+    const inicio = screen.getByText("Início");
+    const lista = screen.getByText("Lista");
+    const gêneros = screen.getByText("Gêneros");
+    const novos = screen.getByText("Novos Episódios");
+
+    expect(inicio).toBeInTheDocument();
+    expect(lista).toBeInTheDocument();
+    expect(gêneros).toBeInTheDocument();
+    expect(novos).toBeInTheDocument();
+  });
+
+  it("Redirects the navigation links correctly", () => {
+    render(<Header />);
+
+    const inicio = screen.getByText("Início");
+    const lista = screen.getByText("Lista");
+    const gêneros = screen.getByText("Gêneros");
+    const novos = screen.getByText("Novos Episódios");
+
+    expect(inicio.getAttribute("href")).toEqual("/");
+    expect(lista.getAttribute("href")).toEqual("/lista");
+    expect(gêneros.getAttribute("href")).toEqual("/generos");
+    expect(novos.getAttribute("href")).toEqual("/novos-episodios");
+  });
+
+  it("Renders search input correctly", () => {
+    render(<Header />);
+
+    const search = screen.getByPlaceholderText("Buscar");
+
+    expect(search).toBeInTheDocument();
   });
 });
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,32 @@
+import { Container } from "./styles";
+
 export const Header: React.FC = () => {
-  return <h1>Animados</h1>;
+  return (
+    <Container>
+      <div className="logo">
+        <a href="/" className="logo__title">
+          Animados
+        </a>
+      </div>
+
+      <div className="navigation">
+        <a href="/" className="navigation_link">
+          Início
+        </a>
+        <a href="/lista" className="navigation_link">
+          Lista
+        </a>
+        <a href="/generos" className="navigation_link">
+          Gêneros
+        </a>
+        <a href="/novos-episodios" className="navigation_link">
+          Novos Episódios
+        </a>
+      </div>
+
+      <div className="search">
+        <input type="search" placeholder="Buscar" className="search__input" />
+      </div>
+    </Container>
+  );
 };
